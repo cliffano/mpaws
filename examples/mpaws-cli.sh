35d5c772390ce8def8b122d5fbc6b4fd71e91ac5
Cliffano Subagio <cliffano@gmail.com>
2025-06-09 18:04:35 +1000

    Combine examples script with standard -cli.sh naming convention.

---
+++ examples/mpaws-cli.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+
+printf "\n\n========================================\n"
+printf "Run command with args when MPAWS_PROFILES and MPAWS_REGIONS are set: mpaws s3 ls\n"
+MPAWS_PROFILES=profile1,profile2,profile3 \
+  MPAWS_REGIONS=us-east-1,ap-southeast-2 \
+  mpaws ec2 describe-instances
+
+printf "\n\n========================================\n"
+printf "Run command with args when MPAWS_PROFILES and MPAWS_REGIONS are set: mpaws s3 ls\n"
+MPAWS_PROFILES=profile1,profile2,profile3 \
+  MPAWS_REGIONS=us-east-1 \
+  mpaws ec2 describe-instances
+
+printf "\n\n========================================\n"
+printf "Run command with args when MPAWS_PROFILES and AWS_DEFAULT_REGION are set: mpaws s3 ls\n"
+MPAWS_PROFILES=profile1,profile2,profile3 \
+  AWS_DEFAULT_REGION=us-east-1 \
+  mpaws ec2 describe-instances
+
+printf "\n\n========================================\n"
+printf "Run command with args when MPAWS_PROFILES and AWS_REGION are set: mpaws s3 ls\n"
+MPAWS_PROFILES=profile1,profile2,profile3 \
+  AWS_REGION=us-east-1 \
+  mpaws ec2 describe-instances
--- examples/multi-profiles-multi-regions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -o nounset
-
-echo "\n\n========================================"
-echo "Run command with args when MPAWS_PROFILES and MPAWS_REGIONS are set: mpaws s3 ls"
-MPAWS_PROFILES=profile1,profile2,profile3 \
-  MPAWS_REGIONS=us-east-1,ap-southeast-2 \
-  mpaws ec2 describe-instances
